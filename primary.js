aad46dd35228cfb5d20aca81061549e49be8c980
motemed khaled <motemedkhaled5@gmail.com>
2023-10-04 19:47:50 +0300

    load balancer issue two

--- primary.js
+++ primary.js
@@ -4,22 +4,28 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const cpuCount = os.cpus().length;
 
-console.log(`the total number of cpu is :${cpuCount}`);
-console.log(`primary pid:${process.pid}`)
+console.log(`The total number of CPUs is: ${cpuCount}`);
+console.log(`Primary PID: ${process.pid}`);
 
-cluster.setupPrimary({
-    exec: __dirname + "/server.js"
-});
+if (cluster.isPrimary) {
+  console.log("Primary process is running");
 
-for (let i = 0; i < cpuCount; i++) {
+  // Fork workers based on the number of CPUs
+  for (let i = 0; i < cpuCount; i++) {
     cluster.fork();
+  }
+
+  // Handle worker exit
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} has been killed`);
+    console.log("Starting another worker");
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker process started with PID: ${process.pid}`);
+
+  // Start your server logic here
+  import("./server.js");
 }
-
-cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} has been killed`);
-    console.log(`starting anthor worker`);
-    cluster.fork();
-})
